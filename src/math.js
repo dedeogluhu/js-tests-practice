4ef0026abc45bb277804f8ba6e1693eb41d28936
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-26 22:47:21 +0300

    Add more math tests

--- src/math.js
+++ src/math.js
@@ -1,7 +1,11 @@
 const functions = {
-    add(number1, number2) {
-        return number1 + number2;
-    }
+    add: (number1, number2) => number1 + number2,
+
+    substract: (number1, number2) => number1 - number2,
+
+    multiply: (number1, number2) => number1 * number2,
+
+    divide: (number1, number2) => number1 / number2
 }
 
 module.exports = functions;
--- tests/math.test.js
+++ tests/math.test.js
@@ -3,3 +3,15 @@ const mathFile = require('../src/math');
 test('Adds 2 + 2 to equal 4', () => {
     expect(mathFile.add(2, 2)).toBe(4);
 });
+
+test('Substracts 4 - 2 to equal to 2', () => {
+    expect(mathFile.substract(4, 2)).toBe(2);
+})
+
+test('Multiplies 2 * 2 to equal to 4', () => {
+    expect(mathFile.multiply(2, 2)).toBe(4);
+})
+
+test('Divides 4 / 2 to equal to 2', () => {
+    expect(mathFile.divide(4, 2)).toBe(2);
+})
