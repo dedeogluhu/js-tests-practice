650d9e9335d514779a5660f058098829c00bb9e5
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-30 23:17:12 +0300

    Add a comment

--- src/referenceTypes.js
+++ src/referenceTypes.js
@@ -1,9 +1,11 @@
 const functions = {
+    //Object
     returnObject: (number) => {
         object = { number }
         return object
     },
 
+    //Array
     returnArray: (num1, num2) => {
         array = [num1, num2];
         return array;
